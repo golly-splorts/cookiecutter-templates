c1daff9fa501edfb13c164b66e4ef69821d69684
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2026-03-01 19:17:54 -0800

    Fix deploy.sh: use github.com HTTPS instead of SSH alias, remove personal identity

--- gollyx-star-{{cookiecutter.lower_shortname}}-pelican/scripts/deploy.sh
+++ gollyx-star-{{cookiecutter.lower_shortname}}-pelican/scripts/deploy.sh
@@ -2,15 +2,13 @@
 
 set -euo pipefail
 
-# This is machine-dependent (SSH config)
-GH_URL="ch4zm.github.com"
 DRY_RUN=""
 
 if [ -z ${GOLLYX_PELICAN_HOME+x} ]; then
 	echo 'You must set the $GOLLYX_PELICAN_HOME environment variable to proceed.'
     echo 'Try sourcing environment.{STAGE}'
 	exit 1
-else 
+else
 	echo "\$GOLLYX_PELICAN_HOME is set to '$GOLLYX_PELICAN_HOME'"
 fi
 
@@ -18,7 +16,7 @@ if [ -z ${GOLLYX_STAGE+x} ]; then
 	echo 'You must set the $GOLLYX_STAGE environment variable to proceed.'
     echo 'Try sourcing environment.{STAGE}'
 	exit 1
-else 
+else
 	echo "\$GOLLYX_STAGE is set to '$GOLLYX_STAGE'"
 fi
 
@@ -50,12 +48,12 @@ case ${GOLLYX_STAGE} in
     ;;
 esac
 
-echo "Cloning repo ${GH_URL}/golly-splorts/${REPO}"
+echo "Cloning repo github.com/golly-splorts/${REPO}"
 
 (
 cd ${GOLLYX_PELICAN_HOME}/pelican
 rm -fr output
-git clone -b gh-pages git@${GH_URL}:golly-splorts/${REPO}.git output
+git clone -b gh-pages https://github.com/golly-splorts/${REPO}.git output
 
 rm -fr output/*
 
@@ -66,11 +64,8 @@ pelican content
 echo "Committing new content..."
 cd output
 
-# Set the username for git commit
-git config user.name "Ch4zm of Hellmouth"
-
-# Set the email for git commit
-git config user.email "ch4zm.of.hellmouth@gmail.com"
+git config user.name "Gollyx Orchestrator"
+git config user.email "orchestrator@golly.life"
 
 echo $DOM > CNAME
 
@@ -92,7 +87,7 @@ if [[ $DRY_RUN == "--dry-run" ]]; then
     echo "Skipping push step, --dry-run flag present"
 else
     echo "Pushing to remote"
-    GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa_ch4zm -o IdentitiesOnly=yes -o StrictHostKeyChecking=no" git push origin gh-pages
+    git push origin gh-pages
 fi
 )
 
